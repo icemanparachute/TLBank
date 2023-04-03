1059d0b16d882056fd0853856291c12506eb8688
icemanparachute <acfmichelsen@gmail.com>
2023-04-03 16:55:13 -0400

    add more params

--- test/test.js
+++ test/test.js
@@ -4,44 +4,41 @@ const adminTests = require('./tests/adminTests');
 const happyPath = require('./tests/happyPath');
 const { createTest, createErrorTests } = require('./tests/createTests');
 const redeemTest = require('./tests/redeemTests');
-const {relockTest, relockErrorTest} = require('./tests/relockTests');
+const { relockTest, relockErrorTest } = require('./tests/relockTests');
+
+const paramsMatrix = [
+  { amount: C.E18_100, unlockShift: 100 },
+  { amount: C.E18_1000, unlockShift: 1800 },
+  { amount: C.E18_05, unlockShift: 1200 },
+  { amount: C.E18_13, unlockShift: 1500 },
+];
 
 describe('it tests the deploy and admin functions', () => {
   adminTests();
 });
 
 describe('It goes through the happy path to deploy, create a TimeLockedBank NFT, relock and then redeem', () => {
-  const paramsMatrix = [{ amount: C.E18_100, unlockShift: 100 }];
   paramsMatrix.forEach((param) => {
     happyPath(param);
   });
 });
 
 describe('The creating tests and create error tests', () => {
-    const paramsMatrix = [
-        {amount: C.E18_1000, unlockShift: 100}
-    ];
-    paramsMatrix.forEach((param) => {
-        createTest(param);
-    });
-    createErrorTests();
+  paramsMatrix.forEach((param) => {
+    createTest(param);
+  });
+  createErrorTests();
 });
 
 describe('Testing for redemption methods', () => {
-    const paramsMatrix = [
-        {amount: C.E18_1000, unlockShift: 100}
-    ];
-    paramsMatrix.forEach((param) => {
-        redeemTest(param);
-    });
+  paramsMatrix.forEach((param) => {
+    redeemTest(param);
+  });
 });
 
 describe('Testing for the relock methods and errors', () => {
-    const paramsMatrix = [
-        {amount: C.E18_1000, unlockShift: 100}
-    ];
-    paramsMatrix.forEach((param) => {
-        relockTest(param);
-    });
-    relockErrorTest();
+  paramsMatrix.forEach((param) => {
+    relockTest(param);
+  });
+  relockErrorTest();
 });
